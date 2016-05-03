089223ec3b7e223479582d8317f22be99973fbd4
sholzer <steffen.holzer@capgemini.com>
2016-05-03 19:55:32 +0200

    #8 Finished PersistentDataProviderInterface And added RestApiProviderInterface as link to the server

--- CitizenApplication/app/providers/data/PersistentDataProviderInterface.ts
+++ CitizenApplication/app/providers/data/PersistentDataProviderInterface.ts
@@ -1,22 +1,72 @@
 import Bus from "../model/Bus";
 import {UpdateData} from "../model/UpdateData";
 import Line from "../model/Line";
+import Stop from "../model/Stop";
+import Route from "../model/Route";
 /**
  * Created by sholzer on 03.05.2016.
  */
 
+export default PersistentDataProviderInterface;
 
+/**
+ * Retrieves data from and stores data in the data store
+ */
 export interface PersistentDataProviderInterface{
 
+    /**
+     * Returns the time stamps of the data store lists
+     * @return UpdateData object from the data storage
+     */
     getLastUpdateTimes():UpdateData;
+
+    /**
+     * Sets the time stamp of the data storage
+     * @param updateTimes : UpdateData
+     */
     putLastUpdateTimes(updateTimes:UpdateData):void;
 
+    /**
+     * @return Array of Bus objects from the data store
+     */
     getBusses():Bus[];
+
+    /**
+     * Stores a list of Bus object in the data storage. This method overwrites an already existing Bus object list in the storage
+     * @param busses :Bus[]
+     */
     putBusses(busses:Bus[]):void;
 
+    /**
+     * @return Array of Line objects from the data store
+     */
     getLines():Line[];
+
+    /**
+     * Stores a list of Line object in the data storage. This method overwrites an already existing Line object list in the storage
+     * @param lines :Line[]
+     */
     putLines(lines:Line[]):void;
 
+    /**
+     * @return Array of Stop objects from the data store
+     */
+    getStops():Stop[];
 
+    /**
+     * Stores a list of Stop object in the data storage. This method overwrites an already existing Stop object list in the storage
+     * @param stops :Stop[]
+     */
+    putStops(stops:Stop[]):void;
 
+    /**
+     * @return Array of Route objects from the data store
+     */
+    getRoutes():Route[];
+
+    /**
+     * Stores a list of Route object in the data storage. This method overwrites an already existing Route object list in the storage
+     * @param routes :Route[]
+     */
+    putRoutes(routes:Route[]):void;
 }
---
+++ CitizenApplication/app/providers/data/RestApiProviderInterface.ts
@@ -0,0 +1,38 @@
+import {UpdateData} from "../model/UpdateData";
+import Bus from "../model/Bus";
+import Line from "../model/Line";
+import Stop from "../model/Stop";
+import Route from "../model/Route";
+/**
+ * Created by sholzer on 03.05.2016.
+ */
+
+
+export interface RestApiProviderInterface{
+
+    /**
+     * @return :UpdateData containing the time stamps of the data on the server
+     */
+    getUpdateDataFromServer():UpdateData;
+
+    /**
+     * @return :Bus[] containing the Bus information from the server
+     */
+    getBussesFromServer():Bus[];
+
+    /**
+     * @return :Line[] containing the Line information from the server
+     */
+    getLinesFromServer():Line[];
+
+    /**
+     * @return :Stop[] containing the Stop information from the server
+     */
+    getStopsFromServer():Stop[];
+
+    /**
+     * @return :Route[] containing the Route information from the server
+     */
+    getRoutesFromServer():Route[];
+
+}
