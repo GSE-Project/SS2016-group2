54d86f5d750d0d9f72e76c625d9bcce43875b57f
Marcel Müller <m_mueller14@cs.uni-kl.de>
2016-05-05 17:52:34 +0200

    #9 Map update, try to find bug fixes

--- CitizenApplication/app/pages/map/map.ts
+++ CitizenApplication/app/pages/map/map.ts
@@ -1,5 +1,5 @@
 import {Page, NavController} from 'ionic-angular';
-
+import {Geolocation} from 'ionic-native';
 /*
   Generated class for the MapPage page.
 
@@ -10,5 +10,26 @@ import {Page, NavController} from 'ionic-angular';
   templateUrl: 'build/pages/map/map.html',
 })
 export class MapPage {
-  constructor(public nav: NavController) {}
+  constructor(public nav: NavController) {
+    this.map = null;
+
+    this.loadMap()
+  }
+
+  loadMap(){
+
+    let options ={timeout: 10000, enableHighAccuracy: true};
+
+    Geolocation.getCurrentPosition(options).then(function(position) {
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mypTypeId: google.maps.MapTypeId.ROADMAP,
+      };
+
+      this.map = new google.maps.Map((<HTMLInputElement>document.getElementById("map")), mapOptions);
+    });
+  }
 }
