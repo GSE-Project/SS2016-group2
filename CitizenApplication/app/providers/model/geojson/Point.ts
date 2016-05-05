fcaae4fa0f5597f2a8946e034835fdf16f9b1d3d
skaldo <skaldo91@gmail.com>
2016-05-05 13:10:46 +0200

    #10 there was an error (Error TS2420: Class 'Point' incorrectly implements interface), fixed.

--- CitizenApplication/app/providers/model/geojson/Point.ts
+++ CitizenApplication/app/providers/model/geojson/Point.ts
@@ -1,5 +1,6 @@
 /**
- * Created by sholzer on 03.05.2016 after GeoJSon specification
+ * Created by sholzer on 03.05.2016 after GeoJSon specification.
+ * Updated & reviewed by skaldo on 05.05.2016.
  */
 
 import GeoJSonObject from './GeoJSonObject.ts';
@@ -7,10 +8,10 @@ import Coordinate from './Coordinate.ts';
 
 export default Point;
 
-export class Point implements GeoJSonObject{
-    type:"Point";
-    coordinates:{latitude: number, longitude: number}[];
-    public constructor(x: number, y: number){
-        this.coordinates = [new Coordinate(x,y)];
+export class Point implements GeoJSonObject {
+    type: "Point";
+    coordinates: { latitude: number, longitude: number }[];
+    public constructor(x: number, y: number) {
+        this.coordinates = [new Coordinate(x, y)];
     }
 }
--- CitizenApplication/app/providers/model/geojson/geojsonObject.ts
+++ CitizenApplication/app/providers/model/geojson/geojsonObject.ts
@@ -1,11 +1,12 @@
 /**
  * Created by sholzer on 03.05.2016.
+ * Updated & reviewed by skaldo on 05.05.2016.
  */
 import Coordinate from './Coordinate';
 
 export default GeoJSonObject;
 
 export interface GeoJSonObject {
-    type:["Positions", "Point", "MultiPoint", "LineString", "MultiLineString", "Polygon", "MultiPolygon", "GeometryCollection"];
-    coordinates:Coordinate[];
+    type: "Positions" | "Point" | "MultiPoint" | "LineString" | "MultiLineString" | "Polygon" | "MultiPolygon" | "GeometryCollection";
+    coordinates: Coordinate[];
 }
