aa1725fd352ba837efb885ca2e3ecc24602d07b1
skaldo <skaldo91@gmail.com>
2016-05-06 19:54:56 +0200

    #13 Added typings for the tests.

--- CitizenApplication/typings/browser.d.ts
+++ CitizenApplication/typings/browser.d.ts
@@ -1,2 +1,6 @@
+/// <reference path="browser/ambient/angular-protractor/index.d.ts" />
 /// <reference path="browser/ambient/es6-shim/index.d.ts" />
 /// <reference path="browser/ambient/google.maps/index.d.ts" />
+/// <reference path="browser/ambient/jasmine/index.d.ts" />
+/// <reference path="browser/ambient/node/index.d.ts" />
+/// <reference path="browser/ambient/selenium-webdriver/index.d.ts" />
