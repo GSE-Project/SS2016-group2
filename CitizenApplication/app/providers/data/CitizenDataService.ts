95015ad4bf9c2d73c2c0b17e518c93eacd2032ff
sholzer <steffen.holzer@capgemini.com>
2016-05-06 16:55:53 +0200

    #8 Corrected some mistakes on the CitizenDataService

--- CitizenApplication/app/providers/data/CitizenDataService.ts
+++ CitizenApplication/app/providers/data/CitizenDataService.ts
@@ -17,17 +17,21 @@ import {timeInterval} from "rxjs/operator/timeInterval";
  * Service class to provide data from the data storage to the app ui
  */
 export class CitizenDataService implements CitizenDataServiceInterface{
-	constructor(){
-		this.cache = this.populateDataCache();
-		this.update();
-	}
 
 
-	constructor(restApi:RestApiProviderInterface, storageApi:PersistentDataProviderInterface){
+	constructor(restApi:RestApiProviderInterface = null, storageApi:PersistentDataProviderInterface=null){
 		this.cache = this.populateDataCache();
-		this.restApi = restApi;
-		this.storageApi = storageApi;
-		this.update();
+		if(restApi == null){
+			/*this.restApi = new Instance*///TODO instance;
+		}else {
+			this.restApi = restApi;
+		}
+		if(storageApi == null){
+			/*this.storageApi = new Instance*///TODO instance
+		}else{
+			this.storageApi = storageApi;
+		}
+		this.populateDataCache();
 	}
 	private timerSet:boolean = false;
 	private restApi: RestApiProviderInterface /*TODO initialize*/ ;
@@ -43,7 +47,7 @@ export class CitizenDataService implements CitizenDataServiceInterface{
 	* @param filter optional parameter to filter the output list
 	* @return A list of Stop object
 	*/
-	getStopList(filter?: Stop): Stop[]{
+	getStopList(filter?: Stop = null): Stop[]{
 		if(filter == null){
 			return this.cache.cached_stops.slice();
 		}
@@ -60,7 +64,7 @@ export class CitizenDataService implements CitizenDataServiceInterface{
 	* @param filter optional parameter to filter the output list
 	* @return A list of Line objects
 	*/
-	getLineList(filter?: Line): Line[] {
+	getLineList(filter?: Line = null): Line[] {
 		if(filter == null){
 			return this.cache.cached_lines.slice();
 		}
@@ -77,7 +81,7 @@ export class CitizenDataService implements CitizenDataServiceInterface{
 	* @param filter optional parameter to filter the output list
 	* @return A list of Bus objects
 	*/
-	getBusList(filter?: Bus): Bus[] {
+	getBusList(filter?: Bus = null): Bus[] {
 		if(filter == null){
 			return this.cache.cached_busses.slice();
 		}
@@ -95,14 +99,14 @@ export class CitizenDataService implements CitizenDataServiceInterface{
 	* @return Object with properties (position:Point) and (delay:number)
 	*/
 	getBusRealTimeData(id: number): { position: Point, delay: number } {
-		return this._restApi.getRealTimeBusData(id);
+		return this.restApi.getRealTimeBusData(id);
 	};
 
 	/**
 	* @param filter optional parameter to filter the output list.
 	* @return A list of Route objects
 	*/
-	getRoutes(filter?: Route): Route[] {
+	getRoutes(filter?: Route=null): Route[] {
 		if(filter == null){
 			return this.cache.cached_routes.slice();
 		}
@@ -122,16 +126,16 @@ export class CitizenDataService implements CitizenDataServiceInterface{
 		var serverTime : UpdateData = this._restApi.getUpdateDataFromServer();
 		var currentCacheTime : UpdateData = this.cache.cached_timestamp;
 		if(serverTime.busses > currentCacheTime.busses){
-			this.cache.cached_busses = this._restApi.getBussesFromServer();
+			this.cache.cached_busses = this.restApi.getBussesFromServer();
 		}
 		if(serverTime.lines > currentCacheTime.lines){
-			this.cache.cached_lines = this._restApi.getLinesFromServer();
+			this.cache.cached_lines = this.restApi.getLinesFromServer();
 		}
 		if(serverTime.routes > currentCacheTime.routes){
-			this.cache.cached_routes = this._restApi.getRoutesFromServer();
+			this.cache.cached_routes = this.restApi.getRoutesFromServer();
 		}
 		if(serverTime.stops > currentCacheTime.stops){
-			this.cache.cached_stops = this._restApi.getStopsFromServer();
+			this.cache.cached_stops = this.restApi.getStopsFromServer();
 		}
 		this.cache.cached_timestamp = serverTime;
 		this.putDataToStorage(this.cache);
@@ -159,11 +163,11 @@ export class CitizenDataService implements CitizenDataServiceInterface{
 	};
 
 	private putDataToStorage(data:CitizenDataCache):void{
-		this._storageApi.putBusses(data.cached_busses);
-		this._storageApi.putLines(data.cached_lines);
-		this._storageApi.putRoutes(data.cached_routes);
-		this._storageApi.putStops(data.cached_stops);
-		this._storageApi.putLastUpdateTimes(data.cached_timestamp);
+		this.storageApi.putBusses(data.cached_busses);
+		this.storageApi.putLines(data.cached_lines);
+		this.storageApi.putRoutes(data.cached_routes);
+		this.storageApi.putStops(data.cached_stops);
+		this.storageApi.putLastUpdateTimes(data.cached_timestamp);
 	}
 
 
