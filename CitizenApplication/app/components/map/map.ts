70066cce9e5f438609bb0a8078fcada1721d4aeb
skaldo <skaldo91@gmail.com>
2016-05-09 18:00:15 +0200

    #9 extending the map component.

---
+++ CitizenApplication/app/components/map/map.ts
@@ -0,0 +1,47 @@
+import {Component, ElementRef, OnInit} from 'angular2/core';
+import {Geolocation} from 'ionic-native';
+
+/*
+  Created by skaldo and mmueller on the 09.05.2016.
+  
+  This is Map component for the CitizenApplication.
+*/
+@Component({
+  selector: 'map',
+  templateUrl: 'build/components/map/map.html'
+})
+export class Map implements OnInit, OnInit {
+  private map: google.maps.Map;
+  private defaultMapOptions = {
+    zoom: 15,
+    mypTypeId: google.maps.MapTypeId.ROADMAP,
+  }
+  private defaultGeoLocationOptions = {
+    timeout: 10000,
+    enableHighAccuracy: true
+  };
+
+  constructor(private element: ElementRef) {
+  }
+
+  centerMap(center?: google.maps.LatLng) {
+    if (!center) {
+      Geolocation.getCurrentPosition(this.defaultGeoLocationOptions).then((position) => {
+        let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        this.centerMap(latLng);
+      });
+      return;
+    }
+    this.map.setCenter(center);
+  }
+
+  createMap() {
+    let element = this.element.nativeElement.querySelector('.mapElement');
+    this.map = new google.maps.Map(element, this.defaultMapOptions);
+  }
+
+  ngOnInit() {
+    this.createMap();
+    this.centerMap();
+  }
+}
