29b207f539fe4a92e82ab12c0d2e3328ad7ababf
sholzer <steffen.holzer@capgemini.com>
2016-05-12 09:24:27 +0200

    #18 #8 managed to produce a working test. For some reasons the mocked promise cannot access test local variables. Changed the assertEqual() to assertEqualJson(). Removed old Jasmine Stubs. Removed old (not working) tests. As well as some pretty printing

--- CitizenApplication/app/specs/providers/data/CitizenDataService.spec.ts
+++ CitizenApplication/app/specs/providers/data/CitizenDataService.spec.ts
@@ -1,12 +1,14 @@
 import CitizenDataServiceInterface from "../../../providers/data/CitizenDataServiceInterface";
 import {RestApiProvider} from "../../../providers/data/RestApiProvider";
 import {PersistentDataProvider} from "../../../providers/data/PersistentDataProvider";
-import {CitizenDataService} from "../../../providers/data/CitizenDataService";
+import {CitizenDataService, CitizenDataCache} from "../../../providers/data/CitizenDataService";
 import {UpdateData} from "../../../providers/model/UpdateData";
 import {Bus} from "../../../providers/model/Bus";
 import {Line} from "../../../providers/model/Line";
 import {Route} from "../../../providers/model/Route";
 import {Stop} from "../../../providers/model/Stop";
+
+import {Http} from "angular2/http";
 /**
  * Created by sholzer on 06.05.2016.
  * Updated by skaldo on 07.05.2016.
@@ -21,45 +23,32 @@ describe("CitizenDataService specifications", function () {
     var storageUpdateData: UpdateData;
     var serverUpdateData: UpdateData;
 
+    var http: any;
+
     var busses: Bus[];
     var lines: Line[];
     var stops: Stop[];
     var routes: Route[];
 
-    var createMockData = function <T>(n: number, type:{new():T}): T[] {
+    /**
+     * Creates an array of data of the type T
+     * @param n length of the Array
+     * @param type Class of the Type this array will contain
+     * @return Array<T> with objects with id = 1...n
+     */
+    var createMockData = function <T>(n: number, type: { new (): T }): T[] {
         var result: T[] = [];
         // und bist du nicht willig ... 
-        for(var i:number = 0; i<n;i++){
+        for (var i: number = 0; i < n; i++) {
             var object = new type();
-            object['id']=i;
+            object['id'] = i;
             result.push(<T>object);
         }
-        
+
         return result;
     }
 
-    beforeEach(function () { //Reset Stubs
-        restApi = jasmine.createSpyObj('restApi', [
-            'getUpdateDataFromServer',
-            'getBussesFromServer',
-            'getLinesFromServer',
-            'getStopsFromServer',
-            'getRoutesFromServer',
-            'getRealTimeBusData'
-        ]);
-
-        storageApi = jasmine.createSpyObj('storageApi', [
-            'getLastUpdateTimes',
-            'putLastUpdateTimes',
-            'getBusses',
-            'putBusses',
-            'getLines',
-            'putLines',
-            'getStops',
-            'putStops',
-            'getRoutes',
-            'putRoutes'
-        ]);
+    beforeEach(() => { //Reset Stubs
 
         var storageUpdateData = new UpdateData();
         storageUpdateData.busses = 1;
@@ -77,21 +66,44 @@ describe("CitizenDataService specifications", function () {
         lines = createMockData<Line>(10, Line);
         stops = createMockData<Stop>(10, Stop);
         routes = createMockData<Route>(10, Route);
-
-        (<jasmine.Spy>storageApi.getLastUpdateTimes).and.returnValue(storageUpdateData);
-        (<jasmine.Spy>storageApi.getBusses).and.returnValue(busses);
-        (<jasmine.Spy>storageApi.getLines).and.returnValue(lines);
-        (<jasmine.Spy>storageApi.getRoutes).and.returnValue(routes);
-        (<jasmine.Spy>storageApi.getStops).and.returnValue(stops);
     });
 
     // Test object
     var citizenDataService: CitizenDataServiceInterface;
     describe("Initialization", function () {
 
+        storageApi = <PersistentDataProvider>{
+            getLastUpdateTimes(): Promise<UpdateData> {
+                var storageUpdateData = new UpdateData();
+                storageUpdateData.busses = 1;
+                storageUpdateData.lines = 1;
+                storageUpdateData.routes = 1;
+                storageUpdateData.stops = 1;
+                var p: Promise<UpdateData> = Promise.resolve(storageUpdateData);
+                debugger;
+                return p;
+            },
+            getBusses(): Promise<Bus[]> {
+                busses = createMockData<Bus>(10,Bus);
+                return Promise.resolve(busses);
+            },
+            getLines(): Promise<Line[]> {
+                return Promise.resolve(lines);
+            },
+            getRoutes(): Promise<Route[]> {
+                return Promise.resolve(routes);
+            },
+            getStops(): Promise<Stop[]> {
+                return Promise.resolve(stops);
+            }
+        };
 
         citizenDataService = new CitizenDataService(restApi, storageApi);
-
+        var cache: CitizenDataCache = (<CitizenDataService>citizenDataService).getCache();
+        it("Fetching busses", () => {
+            assertEqualJson(cache.cached_busses, busses);
+        });
+        /*
         //Expects
         it("Call for busses", function () {
             expect(storageApi.getBusses).toHaveBeenCalled;
@@ -107,74 +119,18 @@ describe("CitizenDataService specifications", function () {
         })
         it("Call for last update time", function () {
             expect(storageApi.getLastUpdateTimes).toHaveBeenCalled;
-        })
-    });
-
-    describe("Update call", function () {
-
-        //Setup
-        var serverBusses = createMockData<Bus>(9, Bus);
-        serverUpdateData = new UpdateData();
-        serverUpdateData.busses = 2;
-        serverUpdateData.lines = 1;
-        serverUpdateData.routes = 1;
-        serverUpdateData.stops = 1;
-
-        beforeEach(() => {
-            (<jasmine.Spy>restApi.getUpdateDataFromServer).and.returnValue(serverUpdateData);
-            (<jasmine.Spy>restApi.getBussesFromServer).and.returnValue(serverBusses);
-        });
-
-        citizenDataService = new CitizenDataService(restApi, storageApi);
-        citizenDataService.update();
-
-        it("Timestamps from server fetched", function () {
-            expect(restApi.getUpdateDataFromServer).toHaveBeenCalled;
-        });
-        it("Updated Bus data fetched", function () {
-            expect(restApi.getBussesFromServer).toHaveBeenCalled;
-        })
-        it("Other entries are not updated", function () {
-            expect(restApi.getLinesFromServer).not.toHaveBeenCalled;
-            expect(restApi.getRoutesFromServer).not.toHaveBeenCalled;
-            expect(restApi.getStopsFromServer).not.toHaveBeenCalled;
-        });
-        it("Stored updated timestamps in storage", function () {
-            expect(storageApi.putLastUpdateTimes).toHaveBeenCalledWith(serverUpdateData);
-        })
-        it("Stored updated busses data in storage", function () {
-            expect(storageApi.putBusses).toHaveBeenCalledWith(serverBusses);
-            /*expect(storageApi.putLines).toHaveBeenCalledWith(lines);
-            expect(storageApi.putRoutes).toHaveBeenCalledWith(routes);
-            expect(storageApi.putStops).toHaveBeenCalledWith(stops);*///Depends on exact implementation
-        });
-    });
-
-    describe("Getter", function () {
-        // Depends on current (2016-05-08, 16:47) implementation of createMockData()
-        var resultBusses: Bus[];
-
-        citizenDataService = new CitizenDataService(restApi, storageApi);
-        it("Get all busses", function () {
-            resultBusses = citizenDataService.getBusList();
-            resultBusses.forEach((b, i) => {
-                if (busses.indexOf(b) == -1) {
-                    fail("Returned list is incomplete");
-                }
-            });
-        });
-        it("Get one bus", function () {
-            var inputBusses: Bus[] = [new Bus(), new Bus()];
-            (<jasmine.Spy>storageApi.getBusses).and.returnValue(inputBusses);
-            var filterBus: Bus;
-            filterBus = new Bus();
-            filterBus.id = 2;
-            if (typeof (filterBus) == "undefined") fail("Input Bus is undefined");
-            resultBusses = citizenDataService.getBusList(filterBus);
-            if (resultBusses.length != 1) fail("Array has the wrong size");
-            if (typeof (resultBusses[0]) == "undefined") fail("No Bus returned. Type was " + typeof (resultBusses[0]));
-            if (resultBusses[0].id != 2) fail("Wrong Bus object returned");
-        });
+        })*/
     });
 
 });
+/**
+ * Tests if the JSON representation of two objects is equal (we don't need the exact reference but only an equal)
+ * @author sholzer 160511 (I wanted an Junit equivalent of assertEquals())
+ * @param input :any an object
+ * @param expectation :any the object input is expected to be equal
+ * @return void. Calls fail() if JSON.stringify(input) != JSON.stringify(expectation)
+ */
+function assertEqualJson(input: any, expectation: any): void {
+    if (JSON.stringify(input) != JSON.stringify(expectation)) fail("Expected\n" + JSON.stringify(input) + "\nto be equal to\n" + JSON.stringify(expectation));
+
+}
