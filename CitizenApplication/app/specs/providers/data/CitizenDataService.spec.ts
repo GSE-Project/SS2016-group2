6e468a05e4880f2fd0edab93a855fe3615ff2c8c
sholzer <s_holzer11@cs.uni-kl.de>
2016-05-14 13:43:44 +0200

    #23 Testing ServerAcces of CitizenDataService. Locally passed so far

--- CitizenApplication/app/specs/providers/data/CitizenDataService.spec.ts
+++ CitizenApplication/app/specs/providers/data/CitizenDataService.spec.ts
@@ -10,6 +10,7 @@ import {Http} from "angular2/http";
 import {IRestStops} from "../../../providers/model/rest/RestStops";
 import {Observable} from "rxjs/Observable";
 import {CitizenDataService} from "../../../providers/data/CitizenDataService";
+import {IBusRealTimeData} from "../../../providers/model/BusRealTimeData";
 /**
  * Created by sholzer on 06.05.2016.
  * Updated by skaldo on 07.05.2016.
@@ -17,41 +18,60 @@ import {CitizenDataService} from "../../../providers/data/CitizenDataService";
 
 describe("CitizenDataService specifications", function () {
    
-    var restApi : RestApiProvider = <RestApiProvider>{
+    var restApi : RestApiProvider;
+    var storageApi : PersistentDataProvider;
+    describe("Get Server Data", ()=>{
 
-        getUpdateData():Observable<IUpdateData>{
-          return Observable.of({
-              busses:1,lines:1,routes:1, stops:1
-          });
-        },
-        getStops():Observable<IRestStops>{
-            return Observable.of({timestamp:1, stops:[{id:1}]});
-        }
-    };
-    var storageApi : PersistentDataProvider = <PersistentDataProvider>{
+        var updateCalled : boolean = false;
 
-        getTimeStamps():IUpdateData{
-            return{
-                busses:0,lines:0,routes:0, stops:0
-            };
-        },
-        getStops():Observable<IRestStops>{
-            return Observable.of({timestamp:0, stops:[]});
-        },
-        putStops(data:IRestStops):void{
-            //nothing
-        }
+        restApi =  <RestApiProvider>{
+            getUpdateData():Observable<IUpdateData>{
+                updateCalled = true;
+                return Observable.of({
+                    busses:1,lines:1,routes:1, stops:1
+                });
+            },
+            getStops():Observable<IRestStops>{
+                return Observable.of({timestamp:1, stops:[{id:1}]});
+            },
+            getRealTimeBusData(id:number):Observable<IBusRealTimeData>{
+                return Observable.of({delay:id, location:{}});
+            }
+        };
+        storageApi  = <PersistentDataProvider>{
+            getTimeStamps():IUpdateData{
+                return{
+                    busses:0,lines:0,routes:0, stops:0
+                };
+            },
+            getStops():Observable<IRestStops>{
+                return Observable.of({timestamp:0, stops:[]});
+            },
+            putStops(data:IRestStops):void{}
+        };
 
-    };
+        var citizenDataService : CitizenDataService = new CitizenDataService(restApi, storageApi);
 
-    var citizenDataService : CitizenDataService = new CitizenDataService(restApi, storageApi);
+        /**
+         * Stops should be a sufficient test since the code base is equivalent for the other model data
+         */
+        it("Get stops from server", ()=>{
+            assertEqualJson(citizenDataService.getStops(), restApi.getStops());
+        });
 
-    it("Get ServerStops", ()=>{
-        assertEqualJson(citizenDataService.getStops(), restApi.getStops());
+        /**
+         * Check the #updateTimeStamps() method.
+         */
+        it("Get new update data", ()=>{
+            updateCalled = false;
+            citizenDataService.updateTimeStamps();
+            assertEqualJson(updateCalled, true);
+        });
+
+        it("Get RealTimeBusData", ()=>{
+            assertEqualJson(citizenDataService.getIBusRealTimeData(1), restApi.getRealTimeBusData(1));
+        });
     });
-
-
-
 });
 
 
