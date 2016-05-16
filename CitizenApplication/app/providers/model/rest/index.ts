391d0290993e11fd5d9263ad66ae545c8c131cb7
skaldo <skaldo91@gmail.com>
2016-05-16 13:07:04 +0200

    #26 added docs to the gp-pages: http://gse-project.github.io/SS2016-group2/

--- CitizenApplication/app/providers/model/rest/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Created by skaldo on 13.05.2016
- */
-
-export * from './RestBusses';
-export * from './RestDataObject';
-export * from './RestLines';
-export * from './RestRoutes';
-export * from './RestStops';
--- CitizenApplication/typings/browser.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="browser/ambient/angular-protractor/index.d.ts" />
-/// <reference path="browser/ambient/es6-shim/index.d.ts" />
-/// <reference path="browser/ambient/google.maps/index.d.ts" />
-/// <reference path="browser/ambient/jasmine/index.d.ts" />
-/// <reference path="browser/ambient/node/index.d.ts" />
-/// <reference path="browser/ambient/selenium-webdriver/index.d.ts" />
