c78366ffac1dbc27ca7466435de4c21ce4e4336e
sholzer <steffen.holzer@capgemini.com>
2016-05-18 22:00:08 +0200

    #18 Added asynchronous tests to DataLogic.spec.ts

--- CitizenApplication/app/specs/integration/DataLogic.spec.ts
+++ CitizenApplication/app/specs/integration/DataLogic.spec.ts
@@ -107,6 +107,95 @@ describe('Data Logic specification (Integration test)', () => {
 
     });
 
+    describe('Delayed access.\n      ! This tests will take some time', () => {
+
+        let storageDelay: number = 500;
+        let storageBusses: IRestBusses = { timestamp: 0, busses: [] };
+        let storageLines: IRestLines = { timestamp: 0, lines: [] };
+        let storageRoutes: IRestRoutes = { timestamp: 0, routes: [] };
+        let storageStops: IRestStops = { timestamp: 0, stops: [] };
+
+        let storage: Storage = <Storage>{
+            get(key: string): Promise<string> {
+                // This should dealy the Promise resolution
+                // praise to https://gist.github.com/joepie91/2664c85a744e6bd0629c
+                return new Promise((resolve) => {
+                    setTimeout(function () {
+                        switch (key) {
+                            case 'B': resolve(storageBusses);
+                            case 'L': resolve(storageLines);
+                            case 'R': resolve(storageRoutes);
+                            case 'S': resolve(storageStops);
+                        }
+                    }, storageDelay);
+                });
+            },
+            set(key: string, value: string): Promise<any> {
+                return Promise.resolve(true);
+            }
+        };
+
+        let serverDelay: number = 1000;
+        let serverBusses: IRestBusses = { timestamp: 0, busses: [] };
+        let serverLines: IRestLines = { timestamp: 1, lines: [] };
+        let serverRoutes: IRestRoutes = { timestamp: 1, routes: [] };
+        let serverStops: IRestStops = { timestamp: 1, stops: [] };
+        let serverUpdateData: IUpdateData = { busses: 0, lines: 1, routes: 1, stops: 1 };
+        let serverBusRealTimeData: IBusRealTimeData = { id: 1, delay: 1, location: { type: GeoJsonObjectTypes.Point, coordinates: [{ longitude: 1, latitude: 1 }] } };
+
+        let http: Http = <Http>{
+            get(url: string): Observable<Response> {
+                let response: Response = new Response(new ResponseOptions({}));
+                if (url.endsWith('busses')) {
+                    response = new Response(new ResponseOptions({ body: serverBusses }));
+                }
+                if (url.endsWith('lines')) {
+                    response = new Response(new ResponseOptions({ body: serverLines }));
+                }
+                if (url.endsWith('routes')) {
+                    response = new Response(new ResponseOptions({ body: serverRoutes }));
+                }
+                if (url.endsWith('stops')) {
+                    response = new Response(new ResponseOptions({ body: serverStops }));
+                }
+                if (url.endsWith('update')) {
+                    response = new Response(new ResponseOptions({ body: serverUpdateData }));
+                }
+                if (url.includes('busses/')) {
+                    response = new Response(new ResponseOptions({ body: serverBusRealTimeData }));
+                }
+                return Observable.fromPromise(new Promise((resolve) => {
+                    setTimeout(function () {
+                        resolve(response);
+                    }, serverDelay);
+                }));
+            }
+        };
+
+        let pdp: PersistentDataProvider = new PersistentDataProvider();
+        let rap: RestApiProvider = new RestApiProvider(http);
+        pdp.setStorage(storage);
+        let cds: CitizenDataService = new CitizenDataService(rap, pdp);
+
+        cds.waitForReady().subscribe(res => {
+            it('Get Stop from Server', () => {
+                cds.getStops().subscribe(data => {
+                    Assert.equalJson(data, serverStops);
+                });
+            });
+            it('Get Busses from Storage', () => {
+                cds.getBusses().subscribe(data => {
+                    Assert.equalJson(data, storageBusses);
+                });
+            });
+            it('Get RealTimeBus data', () => {
+                cds.getBusRealTimeData(1).subscribe(data => {
+                    Assert.equalJson(data, serverBusRealTimeData);
+                });
+            });
+        });
+    });
+
 
 });
 
