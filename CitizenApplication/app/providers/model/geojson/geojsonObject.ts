2df9b75285a90d73b3e272956b5d05dd4ae3c2ad
sholzer <steffen.holzer@capgemini.com>
2016-06-04 15:28:43 +0200

    #66 Import Fixes in model

--- CitizenApplication/app/providers/model/geojson/geojsonObject.ts
+++ CitizenApplication/app/providers/model/geojson/geojsonObject.ts
@@ -1,9 +1,8 @@
 /**
  * Created by sholzer on 03.05.2016.
  * Updated & reviewed by skaldo on 05.05.2016 & 13.05.2016.
+ * Adapted by sholzer on 04.06.2016 due to GitHub Issue #66
  */
-import {Coordinate} from '../';
-
 export default IGeoJsonObject;
 
 export interface IGeoJsonObject {
